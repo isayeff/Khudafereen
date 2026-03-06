703ae6836a40f34253c87cf89eba7c9a271e5682
isayeff <elvin2004ei@gmail.com>
2026-03-06 04:49:04 +0000

    scroll fix

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,18 +1,18 @@
-// SCROLL
-
-// export function scrollToSection(id) {
-//   const el = document.getElementById(id);
-//   if (el) el.scrollIntoView({ behavior: 'smooth', block: 'start' });
-// }
+SCROLL
 
 export function scrollToSection(id) {
   const el = document.getElementById(id);
-  if (!el) return;
-
-  const navbar = document.querySelector('nav');
-  const navbarHeight = navbar ? navbar.offsetHeight : 0; //preventing section title goes under navbar while scroll
-
-  const top = el.getBoundingClientRect().top + window.scrollY - navbarHeight;
-
-  window.scrollTo({ top, behavior: 'smooth' });
+  if (el) el.scrollIntoView({ behavior: 'smooth', block: 'start' });
 }
+
+// export function scrollToSection(id) {
+//   const el = document.getElementById(id);
+//   if (!el) return;
+
+//   const navbar = document.querySelector('nav');
+//   const navbarHeight = navbar ? navbar.offsetHeight : 0; //preventing section title goes under navbar while scroll
+
+//   const top = el.getBoundingClientRect().top + window.scrollY - navbarHeight;
+
+//   window.scrollTo({ top, behavior: 'smooth' });
+// }
